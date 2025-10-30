0b017e64d8ed61b152e97d6127beb5e582e44ea4
Matthew Edmonds-Wilson <a1850372@adelaide.edu.au>
2025-10-30 16:17:58 +1030

    test: add tests for cutoff sizes for parallel_mergesort

--- tests/parallel_mergesort.c
+++ tests/parallel_mergesort.c
@@ -13,6 +13,7 @@ void printA(size_t start, size_t end) {
 	printf("\nA ");
 	for(size_t i = start; i < end+1; i++) {
 		printf("%d ", A[i]);
+		fflush(stdout);
 	}
 }
 
@@ -20,6 +21,7 @@ void printB(size_t start, size_t end) {
 	printf("\nB ");
 	for(size_t i = start; i < end+1; i++) {
 		printf("%d ", B[i]);
+		fflush(stdout);
 	}
 }
 
@@ -62,34 +64,35 @@ int main(void) {
 
 	{
 		printf("Test 2: sort an array in parallel with too many levels\n");	
-		cutoff = 32;
-		A = malloc(10 * sizeof(int));
-		B = malloc(10 * sizeof(int));
-
-		A[0] = 5;
-		A[1] = 7;
-		A[2] = 2;
-		A[3] = 3;
-		A[4] = 7;
-		A[5] = 1;
-		A[6] = 9;
-		A[7] = 8;
-		A[8] = 6;
-		A[9] = 1;
+		cutoff = 16;
+		A = malloc(9 * sizeof(int));
+		B = malloc(9 * sizeof(int));
+		
+		A[5] = 1; // 0
+		A[2] = 2; // 1
+		A[3] = 3; // 2
+		A[0] = 5; // 3
+		A[8] = 6; // 4
+		A[4] = 7; // 5
+		A[1] = 7; // 6
+		A[7] = 8; // 7
+		A[6] = 9; // 8
 
 		struct argument* args = buildArgs(0,9,0);
 		parallel_mergesort(args);
 
+		printA(0,9);
+		printB(0, 9);
+
 		assert(A[0] == 1);
-		assert(A[1] == 1);
-		assert(A[2] == 2);
-		assert(A[3] == 3);
-		assert(A[4] == 5);
-		assert(A[5] == 6);
+		assert(A[1] == 2);
+		assert(A[2] == 3);
+		assert(A[3] == 5);
+		assert(A[4] == 6);
+		assert(A[5] == 7);
 		assert(A[6] == 7);
-		assert(A[7] == 7);
-		assert(A[8] == 8);
-		assert(A[9] == 9);
+		assert(A[7] == 8);
+		assert(A[8] == 9);
 
 		printf("Test 2: Passed\n");
 	}
